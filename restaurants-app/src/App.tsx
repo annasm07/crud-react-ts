0a9436dc68e89d46ff538dec0fefb769031982e9
annasm07 <anna.sala.mercade@gmail.com>
2021-11-19 11:31:41 +0100

    Set up React app with Typescript
    - Remove unnecesary files
    - Install redux, router, eslint and scss libraries
    - Set up eslint for typescript rules

---
+++ restaurants-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = function () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Frontend Challenge
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
