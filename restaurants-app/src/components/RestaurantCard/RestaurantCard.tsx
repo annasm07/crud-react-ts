d8f53fb08d0523e464d745f774d803f1b02f97b9
annasm07 <anna.sala.mercade@gmail.com>
2021-11-20 10:52:30 +0100

    Add styles to Rest Card and get detail informationon rest detail

--- restaurants-app/src/components/RestaurantCard/RestaurantCard.tsx
+++ restaurants-app/src/components/RestaurantCard/RestaurantCard.tsx
@@ -4,12 +4,18 @@ import Restaurant from '../../interfaces/restaurantInterface';
 import './styles.scss';
 
 const RestaurantCard = function ({ restaurant } : {restaurant:Restaurant}) {
+  function calculateMedium(reviewsArray:number[]) {
+    const reviewsSum = reviewsArray.reduce((acc:number, review:number) => (acc + review));
+    return (reviewsSum / reviewsArray.length).toFixed(1);
+  }
+
   return (
     <li className="restaurant-card">
       <Link to={`/restaurant/${restaurant.id}`} className="restaurant-card__item">
         <img src={restaurant.image_url} alt={restaurant.name} className="restaurant-card__img" />
-        <div>
-          <span className="restaurant-card__name">{restaurant.name}</span>
+        <div className="restaurant-card__information">
+          <span className="information__name">{restaurant.name}</span>
+          <span className="information__review">{calculateMedium(restaurant.reviews)}</span>
         </div>
       </Link>
     </li>
--- restaurants-app/src/pages/RestaurantDetail/RestaurantDetail.tsx
+++ restaurants-app/src/pages/RestaurantDetail/RestaurantDetail.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Restaurant from '../../interfaces/restaurantInterface';
 
 const RestaurantDetail = function () {
+  const restaurants = useSelector((store:any) => store.restaurants);
+
   const { restaurantId } = useParams();
+  const currentRestaurant = restaurants.filter(
+    (restaurant:Restaurant) => restaurant.id === Number(restaurantId),
+  );
   return (
     <p>
       This is the detail Page
       {' '}
-      {restaurantId}
+      {currentRestaurant[0].name}
     </p>
   );
 };
